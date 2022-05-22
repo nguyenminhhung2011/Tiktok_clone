ceb9a62fd49c145e1fd0cdcd18aa4e1370c9acaf
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2022-05-22 13:24:33 +0700

    add name for message

--- lib/screens/Notification/widget/recCard.dart
+++ lib/screens/Notification/widget/recCard.dart
@@ -37,91 +37,116 @@ class RecCard extends StatelessWidget {
               ),
             ),
             const SizedBox(width: 5),
-            (data.typeOfMessage == 0)
-                ? Container(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                (data.border1 == 30)
+                    ? Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          Text(
+                            data.username,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Container(),
+                (data.typeOfMessage == 0)
+                    ? Container(
 //          margin: const EdgeInsets.only(left: 10),
-                    decoration: BoxDecoration(
-                      color: color,
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(30),
-                        topLeft: Radius.circular(data.border1 as double),
-                        bottomRight: Radius.circular(30),
-                        bottomLeft: Radius.circular(data.border2 as double),
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
-                    child: Text(
-                      data.tittle,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                      ),
-                    ),
-                  )
-                : (data.typeOfMessage == 1)
-                    ? InkWell(
-                        onTap: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) => Dialog(
-                              backgroundColor: Colors.transparent,
-                              child: Container(
-                                width: MediaQuery.of(context).size.width - 10,
-                                height: MediaQuery.of(context).size.height / 3,
-                                decoration: BoxDecoration(
-                                  color:
-                                      const Color.fromARGB(255, 255, 252, 227),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Container(
-                                  width: MediaQuery.of(context).size.width - 20,
-                                  height:
-                                      MediaQuery.of(context).size.height / 3 -
-                                          10,
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 5, vertical: 5),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(20),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(data.tittle),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          height: MediaQuery.of(context).size.height / 5,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 222, 236, 238),
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(30),
-                              topLeft: Radius.circular(data.border1 as double),
-                              bottomRight: Radius.circular(30),
-                              bottomLeft:
-                                  Radius.circular(data.border2 as double),
-                            ),
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(data.tittle),
-                            ),
+                        decoration: BoxDecoration(
+                          color: color,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            topLeft: Radius.circular(data.border1 as double),
+                            bottomRight: Radius.circular(30),
+                            bottomLeft: Radius.circular(data.border2 as double),
+                          ),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 12),
+                        child: Text(
+                          data.tittle,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
                           ),
                         ),
                       )
-                    : Container(
-                        height: 50,
-                        width: 50,
-                        child: Image(
-                          fit: BoxFit.fill,
-                          image: NetworkImage(data.tittle),
-                        ),
-                      ),
+                    : (data.typeOfMessage == 1)
+                        ? InkWell(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => Dialog(
+                                  backgroundColor: Colors.transparent,
+                                  child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width - 10,
+                                    height:
+                                        MediaQuery.of(context).size.height / 3,
+                                    decoration: BoxDecoration(
+                                      color: const Color.fromARGB(
+                                          255, 255, 252, 227),
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Container(
+                                      width: MediaQuery.of(context).size.width -
+                                          20,
+                                      height:
+                                          MediaQuery.of(context).size.height /
+                                                  3 -
+                                              10,
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 5, vertical: 5),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(20),
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(data.tittle),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              height: MediaQuery.of(context).size.height / 5,
+                              width: MediaQuery.of(context).size.width / 2.2,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 222, 236, 238),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(30),
+                                  topLeft:
+                                      Radius.circular(data.border1 as double),
+                                  bottomRight: Radius.circular(30),
+                                  bottomLeft:
+                                      Radius.circular(data.border2 as double),
+                                ),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(data.tittle),
+                                ),
+                              ),
+                            ),
+                          )
+                        : Container(
+                            height: 50,
+                            width: 50,
+                            child: Image(
+                              fit: BoxFit.fill,
+                              image: NetworkImage(data.tittle),
+                            ),
+                          ),
+              ],
+            ),
           ],
         ),
       ),
